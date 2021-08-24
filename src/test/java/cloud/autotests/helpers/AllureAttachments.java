efb7d059677a855f0d0d35aef2394ede6bf37f5c
Roman-1990 <84563341+Roman-1990@users.noreply.github.com>
2021-08-24 14:12:38 +0300

    Update AllureAttachments.java

--- src/test/java/cloud/autotests/helpers/AllureAttachments.java
+++ src/test/java/cloud/autotests/helpers/AllureAttachments.java
@@ -10,27 +10,23 @@ import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 public class AllureAttachments {
 
-    public class Attach {
     @Attachment(value = "{attachName}", type = "text/plain")
-    public static String attachAsText(String attachName, String message) {
-        return message;
+    public static void attachAsText(String attachName, String message) {
     }
 
     @Attachment(value = "Page source", type = "text/plain")
-    public static byte[] pageSource() {
-        return getWebDriver().getPageSource().getBytes(StandardCharsets.UTF_8);
+    public static void pageSource() {
+        getWebDriver().getPageSource();
     }
 
     @Attachment(value = "{attachName}", type = "image/png")
-    public static byte[] screenshotAs(String attachName) {
-        return ((TakesScreenshot) getWebDriver()).getScreenshotAs(OutputType.BYTES);
+    public static void screenshotAs() {
+        ((TakesScreenshot) getWebDriver()).getScreenshotAs(OutputType.BYTES);
     }
 
     @Attachment(value = "Video", type = "text/html", fileExtension = ".html")
-    public static String attachVideo(String sessionId) {
-        return "<html><body><video width='100%' height='100%' controls autoplay><source src='"
-                + Browserstack.videoUrl(sessionId)
-                + "' type='video/mp4'></video></body></html>";
+    public static void attachVideo(String sessionId) {
+        Browserstack.videoUrl(sessionId);
     }
 
     public static String getSessionId() {
@@ -42,5 +38,5 @@ public class AllureAttachments {
                 "Browser console logs",
                 String.join("\n", Selenide.getWebDriverLogs(BROWSER))
         );
-
+    }
 }
